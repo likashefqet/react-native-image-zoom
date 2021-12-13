cf2d5ca6a53d93b4dc409f624c94c5daaac7f21b
Shefqet Lika <22661589+likashefqet@users.noreply.github.com>
2021-12-13 04:16:52 +0100

    Upgrade example app to RN 0.66.4 (#4)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
-import { ImageZoom } from 'react-native-image-zoom';
+import { ImageZoom } from '@likashefqet/react-native-image-zoom';
 
 // Photo by Walling [https://unsplash.com/photos/XLqiL-rz4V8] on Unsplash [https://unsplash.com/]
 const imageUri = 'https://images.unsplash.com/photo-1596003906949-67221c37965c';
@@ -19,9 +19,13 @@ const styles = StyleSheet.create({
 function App() {
   return (
     <>
-      <StatusBar hidden />
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <ImageZoom
-        uri={imageUri + `?date=${new Date()}`}
+        uri={imageUri}
         containerStyle={styles.container}
         activityIndicatorProps={{
           color: 'white',
